97c296a1be66cdbc447bcc063a328bd1abce5e24
gabriel montagné láscaris-comneno <gabriel@tibas.london>
2024-06-13 01:18:52 +0200

    stub parse conversation

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 #!/usr/bin/env node
 
+import { parseConversation } from "./parse";
+
 export function main() {
     console.log("uryyb JBEYQ!");
+    console.log(parseConversation("UV"));
 }
 
 if (require.main === module) {
---
+++ src/parse.ts
@@ -0,0 +1,5 @@
+
+export function parseConversation(text: string): string {
+    console.log(`PARSE: ${text}`);
+    return 'BX';
+}
