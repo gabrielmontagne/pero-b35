9adaf6e0635b06bed9b5fc8ed1a2e3321fa292bb
gabriel montagné láscaris-comneno <gabriel@tibas.london>
2024-07-04 05:07:59 +0200

    sketch initial skip

--- src/lib/parse.spec.ts
+++ src/lib/parse.spec.ts
@@ -1,14 +1,39 @@
 import { describe, expect, it } from '@jest/globals';
-import { parse } from './parse';
+import { parse, startEndSplit } from './parse';
 
 
 describe('parse', () => {
   it('should parse a string', () => {
     const result = parse('foo');
     expect(result).toEqual(
-    [
+      [
         { role: 'user' as const, content: 'foo' }
-    ]
+      ]
+    );
+  });
+});
+
+describe('startEndSplit', () => {
+  it('should split a string into leading, main, and trailing', () => {
+    const result = startEndSplit('foo');
+    expect(result).toEqual(
+      { main: 'foo' }
+    );
+  });
+
+  it('should split a string into leading, main, and trailing', () => {
+    const result = startEndSplit(
+`AAAA
+___START___
+BBBB
+___END___
+CCCC`);
+    expect(result).toEqual(
+      {
+        leading: 'AAAA\n',
+        main: 'BBBB\n',
+        trailing: 'CCCC'
+      }
     );
   });
 });
--- src/lib/parse.ts
+++ src/lib/parse.ts
@@ -1,13 +1,30 @@
 import { OperatorFunction, map } from "rxjs";
 import { Session } from "./scan";
 
+const startMarker = /^___START___\s*\n/m
+const endMarker = /^___END___\s*\n/m
+
 export function parse(text: string): Session {
-    return [
-        { role: 'user' as const, content: text }
-    ]
+  return [
+    { role: 'user' as const, content: text }
+  ]
 }
 
-export function parseSession():OperatorFunction<string, Session> {
+export function startEndSplit(text: string): { leading?: string, main: string, trailing?: string } {
+
+  const leadingChunks = text.split(startMarker)
+  const tail = leadingChunks.pop()
+  const trailingChunks = tail?.split(endMarker)
+  const main = trailingChunks?.shift() || text
+
+  return {
+    main,
+    ...leadingChunks.length && { leading: leadingChunks.join('___START___\n') },
+    ...trailingChunks?.length && { trailing: trailingChunks.join('___END___\n') }
+  }
+}
+
+export function parseSession(): OperatorFunction<string, Session> {
   return source$ => source$.pipe(
     map(parse)
   )
