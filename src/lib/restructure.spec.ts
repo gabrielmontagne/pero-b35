6ba53c5aa7412548f82f5e11242074f9afdb38cb
gabriel montagné láscaris-comneno <gabriel@tibas.london>
2025-02-26 23:05:12 -0600

    move to vitest

--- src/lib/restructure.spec.ts
+++ src/lib/restructure.spec.ts
@@ -1,4 +1,4 @@
-import { describe, expect, it } from '@jest/globals'
+import { describe, expect, it } from 'vitest'
 import { TestScheduler } from 'rxjs/testing'
 import { parse, recombineSession, startEndSplit } from './restructure'
 
